3bd583d13cefc9aee0fba7f7be1f6f21c9f8430b
Moulik Chaturvedi <moulik.chaturvedi26@gmail.com>
2021-04-02 15:25:03 +0530

    Changed Data

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,10 +17,10 @@ let header = $(`
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-   <li class="nav-item"><a class="nav-link" href="experience.html">Experience</a></li>
+   <!--<li class="nav-item"><a class="nav-link" href="experience.html">Experience</a></li>-->
    <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
    <!--<li class="nav-item"><a class="nav-link" href="research.html">Research</a></li>-->
-   <li class="nav-item"><a class="nav-link" href="assets/resume.pdf" target="_blank">Resume</a></li>
+   <li class="nav-item"><a class="nav-link" href="assets/Resume.pdf" target="_blank">Resume</a></li>
    <li class="nav-item"><a class="nav-link" href="education.html">Education</a></li>
    <div class="bike">
    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-80 0 650 400" preserveAspectRatio="xMinYMin meet"> 
